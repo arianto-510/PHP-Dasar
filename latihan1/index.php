28a0c16d9792149f377df29855681233deb3b085
Arianto <97211033@student.itk.ac.id>
2022-06-17 21:55:09 +0800

    hapus data

--- latihan1/index.php
+++ latihan1/index.php
@@ -1,3 +1,41 @@
-<?php 
+<?
 
-echo "halo dunia";
+$conn = mysqli_connect("localhost", "root", "", "kuliah");
+$result = mysqli_query($conn, 'select * from mahasiswa');
+
+var_dump($result);
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Website</title>
+</head>
+
+<body>
+    <table border="1" cellpadding="10" cellspacing="0">
+        <tr>
+            <td>Nama</td>
+            <td>NIM</td>
+            <td>Alamat</td>
+            <td>Email</td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>
+
+</body>
+
+</html>
